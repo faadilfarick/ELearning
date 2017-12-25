99c2f3c791c7b3faf9aa1257b50ee439eb99cc46
gofaadil <gofaadil@live.com>
2017-12-25 22:48:36 +0530

    Realtime chat with SignalR

--- ELearning/Controllers/CoursesController.cs
+++ ELearning/Controllers/CoursesController.cs
@@ -288,6 +288,12 @@ namespace ELearning.Controllers
             return RedirectToAction("Index");
         }
 
+        // SignalIR Chat
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- ELearning/Startup.cs
+++ ELearning/Startup.cs
@@ -1,14 +1,24 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using ELearning;
 
-[assembly: OwinStartupAttribute(typeof(ELearning.Startup))]
+//[assembly: OwinStartupAttribute(typeof(ELearning.Startup))] //this is the original one
+[assembly: OwinStartup(typeof(ELearning.Startup))] //this is for signalR but both works fine
 namespace ELearning
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+
+            //ConfigureAuth(app); 
+            //Commenting this will take of user authentication..This is the major problem here if this part is uncommented
+            //signalR will not get the name
+            //nor display chat messages
+
+            
         }
     }
 }
